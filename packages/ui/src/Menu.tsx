d31a2f8e3a1d8d3034808a6cd133916ada33c330
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-04 01:29:41 +0100

    1.0.3 SUPERDATAS

--- packages/ui/src/Menu.tsx
+++ packages/ui/src/Menu.tsx
@@ -48,7 +48,7 @@ export const Menu = () => {
           {" "}
           <button className="hidden py-2 px-4 font-bold text-white md:block">
             <a
-              href="http://twitter.com/miguelgargallo"
+              href="http://twitter.com/superdatas"
               target="_blank"
               rel="noopener noreferrer"
             >
