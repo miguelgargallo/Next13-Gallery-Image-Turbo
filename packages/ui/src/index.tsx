e0acf2f768047ac4fc6d5a17f15defc1c9b6e6aa
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-12-04 13:54:57 +0100

    1.0.0 fixed maybe

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,18 +1,2 @@
 import * as React from "react";
-// Menu
-export * from "./Menu";
-export * from "./MenuPylar";
-// Title and subtitle
-export * from "./TitlePylar";
-// List
-export * from "./ListPylar";
-// Version
-export * from "./Version";
-// Footer
-export * from "./Footer";
-// Other
-export * from "./Scrollable";
-/* Blog Pylar */
-export * from "./BlogPylar/blog";
-export * from "./BlogPylar/post12032022";
-/* Maths */
+export * from "./Button";
