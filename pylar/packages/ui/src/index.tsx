e847f683cf71d4b391b1e0ad3706be513d968578
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-12-04 14:26:35 +0100

    1.0.0 fixed maybe

---
+++ pylar/packages/ui/src/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+// Menu
+export * from "./Menu";
+export * from "./MenuPylar";
+// Title and subtitle
+export * from "./TitlePylar";
+// List
+export * from "./ListPylar";
+// Version
+export * from "./Version";
+// Footer
+export * from "./Footer";
+// Other
+export * from "./Scrollable";
+/* Blog Pylar */
+export * from "./BlogPylar/blog";
+export * from "./BlogPylar/post12032022";
+/* Maths */
