22341fe805308bcc10b72621fc7959f89903cb9f
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-05 20:56:13 +0100

    Update Menu.tsx

--- packages/ui/src/Menu.tsx
+++ packages/ui/src/Menu.tsx
@@ -3,6 +3,7 @@ import { motion, useScroll } from "framer-motion";
 import "./styles.css";
 import { generateJSXMeshGradient } from "meshgrad";
 import { Supermenu } from "./Supermenu";
+import { MenuLogo } from "ui";
 const ELEMENTS = 12;
 
 export const Menu = () => {
@@ -15,7 +16,7 @@ export const Menu = () => {
         style={{ scaleX: scrollYProgress }}
       />
 
-
+<MenuLogo />
       <div className="flex flex-row items-center justify-between bg-gradient-to-r from-black to-gray-800">
         <button className="rounded-xl py-2 px-4 font-bold text-white">
           <a href="/what-is-pylar">What is Pylar</a>
