c49268089ba55693ed1dafc333264638c33e4592
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-18 14:39:05 +0100

    2.0.11 Added more buttons to collections page

--- apps/web/src/pages/collections/index.tsx
+++ apps/web/src/pages/collections/index.tsx
@@ -40,8 +40,18 @@ export default function Home() {
               </a>
             </button>
             <button className="m-2 justify-center rounded-2xl bg-blue-600 py-3 px-4 text-xs font-bold text-white shadow-md hover:shadow-xl md:block">
-              <a className="text-center" href="/collections/girls">
-                Girls
+              <a className="text-center" href="/collections/human">
+                Human
+              </a>
+            </button>
+            <button className="m-2 justify-center rounded-2xl bg-blue-600 py-3 px-4 text-xs font-bold text-white shadow-md hover:shadow-xl md:block">
+              <a className="text-center" href="/collections/box">
+                Box
+              </a>
+            </button>
+            <button className="m-2 justify-center rounded-2xl bg-blue-600 py-3 px-4 text-xs font-bold text-white shadow-md hover:shadow-xl md:block">
+              <a className="text-center" href="/collections/cats">
+                Cats
               </a>
             </button>
           </div>
