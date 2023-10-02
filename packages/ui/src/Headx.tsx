654b60de04e5a859ad36a0df46933302d707d086
miguelgargallo <it@itamaesan.org>
2023-10-02 21:33:22 +0200

    gh 0.0.0 Umami 2

--- packages/ui/src/Headx.tsx
+++ packages/ui/src/Headx.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head';
+
 export const Headx = () => {
     return (
-        <head>
+        <Head>
             <title>Pylar AI by Pencil</title>
             <script async src="https://umami.decentralass.com/script.js" data-website-id="ee2353f0-e6b0-4086-a2ff-22236447f399"></script>
             <link rel="icon" href="./favicon.ico" />
-        </head>
+        </Head>
     );
 };
