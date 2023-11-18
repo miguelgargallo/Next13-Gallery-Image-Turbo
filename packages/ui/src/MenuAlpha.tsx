9b2cef9d51f5496f0ba420ace10368be2adc7fd4
miguelgargallo <it@itamaesan.org>
2023-11-18 02:47:36 +0100

    Remove unused button in MenuAlpha component

--- packages/ui/src/MenuAlpha.tsx
+++ packages/ui/src/MenuAlpha.tsx
@@ -14,10 +14,6 @@ export const MenuAlpha = () => {
         <a href="https://itamaesan.org"
         >ITAMAESAN</a>
       </button>
-      <button className="px-4 py-2 font-bold text-white">
-        <a href="https://superdatas.org"
-        >SuperData</a>
-      </button>
     </div>
   );
 };
